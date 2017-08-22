f26fae556e1e7922fb7bbbcf00230c02eff000b1
mstclair <stclairm@spu.edu>
2017-08-21 17:03:34 -0700

    needed to get the article to populate when local storage is empty, got it

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -57,15 +57,16 @@ Article.fetchAll = function() {
     Article.loadAll(newData);
     console.log('here is the data');
     //TODO: What method do we call to render the index page?
-    articleView.initIndexPage(); 
+    articleView.initIndexPage();
 
   } else {
     // DONE: When we don't already have the rawData,
     $.getJSON('data/hackerIpsum.json').then(
      function(data){
        localStorage.setItem('rawData', JSON.stringify(data));
-
        Article.loadAll(data);
+       articleView.initIndexPage();
+
      });
       // we need to retrieve the JSON file from the server with AJAX (which jQuery method is best for this?),
     // cache it in localStorage so we can skip the server call next time,
